2cc48e6d3af72945975fdb98dce2dec703b33c67
Long Nguyen <18521044@gm.uit.edu.vn>
2022-03-18 11:11:34 +0700

    Updated sql code to have "" instead of ''

--- src/api/controllers/productCtrl.ts
+++ src/api/controllers/productCtrl.ts
@@ -1,5 +1,5 @@
 import { getSortOrder, validateProductInput } from "../../helpers/productHelpers"
-import { db } from '../../config/database'
+import { db } from "../../config/database"
 
 //  GET
 const getProductListing = async (req: any, res: any) => {
@@ -25,7 +25,7 @@ const getProductListing = async (req: any, res: any) => {
 
 const getProduct = async (req: any, res: any) => {
     try {
-        const sql = `SELECT * FROM product WHERE code = '${req.params.code}'`
+        const sql = `SELECT * FROM product WHERE code = "${req.params.code}"`
         let query = db.query(sql, (err, product) => {
             if (err) throw err
             if (product.length < 1) return res.status(500).json("No product found")
@@ -66,10 +66,10 @@ const createProduct = async (req: any, res: any) => {
         const { code, name, category, brand, type, description } = productInput
         const created_at = Date().toString()
         const sql = `INSERT INTO product (code, name, category, brand, type, description, created_at)
-                     VALUES ('${code}', '${name}', '${category}', '${brand}', '${type}', '${description}', '${created_at}')`
+                     VALUES ("${code}", "${name}", "${category}", "${brand}", "${type}", "${description}", "${created_at}")`
         let query = db.query(sql, (err, result) => {
             if (err) throw err
-            return res.status(200).json('Product created successfully')
+            return res.status(200).json("Product created successfully")
         })
     } catch (error) {
         console.log(error)
@@ -92,17 +92,17 @@ const updateProduct = async (req: any, res: any) => {
         const updated_at = Date().toString()
         const sql = `
                         UPDATE product 
-                        SET name = '${name}', 
-                            category = '${category}', 
-                            brand = '${brand}', 
-                            type = '${type}', 
-                            description = '${description}', 
-                            updated_at = '${updated_at}'
-                        WHERE code = '${code}'
+                        SET name = "${name}", 
+                            category = "${category}", 
+                            brand = "${brand}", 
+                            type = "${type}", 
+                            description = "${description}", 
+                            updated_at = "${updated_at}"
+                        WHERE code = "${code}"
                     `
         let query = db.query(sql, (err, result) => {
             if (err) throw err
-            return res.status(200).json('Product updated successfully')
+            return res.status(200).json("Product updated successfully")
         })
     } catch (error) {
         console.log(error)
@@ -119,11 +119,11 @@ const deleteProduct = async (req: any, res: any) => {
         }
         const sql = `
                         DELETE FROM product
-                        WHERE code = '${code}'
+                        WHERE code = "${code}"
                     `
         let query = db.query(sql, (err, result) => {
             if (err) throw err
-            return res.status(200).json('Product deleted successfully')
+            return res.status(200).json("Product deleted successfully")
         })
     } catch (error) {
         console.log(error)
